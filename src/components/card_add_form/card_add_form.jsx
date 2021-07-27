c9946364b6e1c367c9ae4c7829f0d86e5309a2bd
leesh7048 <tmdgus7048@naver.com>
2021-07-27 18:42:30 +0900

    add memo, useCallback

--- src/components/card_add_form/card_add_form.jsx
+++ src/components/card_add_form/card_add_form.jsx
@@ -1,8 +1,8 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, memo } from "react";
 import styles from "./card_add_form.module.css";
 import Button from "../button/button";
 
-const CardAddForm = ({ onAdd, FileInput }) => {
+const CardAddForm = memo(({ onAdd, FileInput }) => {
   const formRef = useRef();
   const nameRef = useRef();
   const companyRef = useRef();
@@ -87,6 +87,6 @@ const CardAddForm = ({ onAdd, FileInput }) => {
       <Button name="Add" onClick={onSubmit} />
     </form>
   );
-};
+});
 
 export default CardAddForm;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import Editor from "../editor/editor";
 import Footer from "../footer/footer";
@@ -11,9 +11,9 @@ const Maker = ({ authService, FileInput, cardRepository }) => {
   const historyState = history?.location?.state;
   const [cards, setCards] = useState({});
   const [userId, setUserId] = useState(historyState && historyState.id);
-  const onLogout = () => {
+  const onLogout = useCallback(() => {
     authService.logout();
-  };
+  }, [authService]);
   useEffect(() => {
     if (!userId) {
       return;
